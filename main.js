1662cc3b1429936a120d9ed31be5ccdfa7e3a7b1
Matt Haggard <haggardii@gmail.com>
2020-07-27 16:36:02 -0400

    Set PATH ahead of time

--- main.js
+++ main.js
@@ -5,11 +5,12 @@ const os = require('os');
 
 async function installWithChoosenim(nimversion) {
   const choosenim_path = await tc.downloadTool('https://nim-lang.org/choosenim/init.sh');
-  let env = Object.assign({}, process.env, {'CHOOSENIM_NO_ANALYTICS': '1'});
-  await exec.exec('sh', [choosenim_path, '-y'], {env});
   const newpath = os.homedir() + '/.nimble/bin';
   core.addPath(newpath);
+  let env = Object.assign({}, process.env, {'CHOOSENIM_NO_ANALYTICS': '1'});
   env = Object.assign({}, env, {'PATH': newpath + ':' + env.PATH});
+
+  await exec.exec('sh', [choosenim_path, '-y'], {env});
   await exec.exec('choosenim', [nimversion], {env});
 }
 
