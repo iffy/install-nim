6a8fb29954d6b5a92d3392b4b465ebfd9aef5c76
Matt Haggard <haggardii@gmail.com>
2023-09-21 21:38:33 -0400

    CI

--- install-nim.sh
+++ install-nim.sh
@@ -51,7 +51,11 @@ EOF
 }
 set +x
 abspath() {
-  echo "$(cd "$(dirname "$1")"; pwd)/$(basename "$1")"
+  echo "Computing abspath of $1" 1>&2
+  python -c "import os; import sys; print(os.path.realpath(sys.argv[1]))" "$1" 1>&2
+  RET="$(echo "$(cd "$(dirname "$1")"; pwd)/$(basename "$1")")"
+  echo "$RET" 1>&2
+  echo "$RET"
 }
 
 NIMDIR=${NIMDIR:-nimdir}
